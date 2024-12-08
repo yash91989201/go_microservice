640bc2542cd0e876862fc620cbbc4bbd3d1007bf
yash91989201 <yashraj6370@gmail.com>
2024-12-08 17:36:56 +0530

    add chi router will /hello route and logging with middleware

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
+	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/hello", basicHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
